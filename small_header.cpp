5480adf9157b2b8556342dcd682e9d70840320bc
Mashrur Rashik <mashrur639@gmail.com>
2018-06-18 16:55:46 +0600

    Create small_header.cpp

---
+++ small_header.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define clr(x)              memset(x, 0, sizeof(x))
+#define clrDp(x)            memset(x, -1, sizeof(x))
+#define ALL(x)              x.begin(), x.end()
+#define pii                 pair <ll,ll>
+#define inf                 (ll) 9e18
+#define eps                 1e-9
+typedef long long ll;
