63002aa793b03377ab8ca34f16dc8190567ee2e6
Mashrur Rashik <mashrur639@gmail.com>
2018-06-18 16:58:33 +0600

    Update small_header.cpp

--- small_header.cpp
+++ small_header.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define eps       1e-9
+#define inf       (ll) 9e18
+#define pii       pair <ll,ll>
+#define ALL(x)    x.begin(), x.end()
 #define clr(x)    memset(x, 0, sizeof(x))
 #define clrDp(x)  memset(x, -1, sizeof(x))
-#define ALL(x)    x.begin(), x.end()
-#define pii       pair <ll,ll>
-#define inf       (ll) 9e18
-#define eps       1e-9
 typedef long long ll;
 
 const int maxi = 111;
